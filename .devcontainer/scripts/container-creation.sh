bd0fbbaa90aa570b44dbbca2956054b8c4b06623
asabla <sparven89@gmail.com>
2022-08-10 20:19:10 +0200

    Re-wrote some workflows for github pages publishing
    Initial workflow file, hopefully it will work

--- .devcontainer/scripts/container-creation.sh
+++ .devcontainer/scripts/container-creation.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-## build repository, so ready to work. 
-dotnet build 
+## build repository, so ready to work.
+dotnet build
 
 ## Add .NET Dev Certs to environment to facilitate debugging
 dotnet dev-certs https
